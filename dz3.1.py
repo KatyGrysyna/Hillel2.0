ec2e73e51a5d6ea281d1ef5a4e0e592e6decf1f7
KatyGrysyna <katerinkakc@gmail.com>
2026-07-22 16:45:37 +0300

    add dz3.1

---
+++ dz3.1.py
@@ -0,0 +1,10 @@
+def get_string_length(text):
+    return len(text)
+
+def join_strings(str1, str2):
+    return str1 + " " +  str2
+
+#перевірка
+
+print(get_string_length("Hello"))
+print(join_strings("Hello", "World"))
